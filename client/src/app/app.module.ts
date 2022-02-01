62871811ed70db7f094b8b66b6ca186d748c5668
Prasann Pradeep Patil <prasannpatil98@gmail.com>
2022-02-01 18:28:17 -0500

    Created Flight Listing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FlightListingComponent } from './flight-listing/flight-listing.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FlightListingComponent
   ],
   imports: [
     BrowserModule,
