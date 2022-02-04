7bc54979842abade199d1f0144bc8bb33270ae10
Prasann Pradeep Patil <prasannpatil98@gmail.com>
2022-02-04 02:05:43 -0500

    added beautification to components

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  navButtonChoice: string= "flight"
+  loginButtonChoice = "login"
 
+  @Output() messageEvent = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+
+  sendMessage() {
+    this.messageEvent.emit(this.navButtonChoice)
+  }
+
+  
+
 
 }
