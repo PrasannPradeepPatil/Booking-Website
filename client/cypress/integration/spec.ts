c32f6d51dbd023f3c2d72260be1be18c082150b9
Prasann Pradeep Patil <prasannpatil98@gmail.com>
2022-03-04 23:12:54 -0500

    added automation test cases

--- client/cypress/integration/spec.ts
+++ client/cypress/integration/spec.ts
@@ -38,4 +38,9 @@ describe("Flight page", () => {
     cy.get("input[id='invalidCheck2']").click()
 
   });
+
+  it.only("Select Ticket Type", () => {
+    cy.get("div[id='economy']").click()
+    cy.get("button[name='next']").click()
+  });
 });
