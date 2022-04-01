c8f2e53ee5d167c679c6f9d88e483a5a6f0fe674
Prasann Pradeep Patil <prasannpatil98@gmail.com>
2022-03-31 21:51:42 -0400

    changed hotel-listing UI

--- client/src/app/hotel-listing/hotel-listing.component.ts
+++ client/src/app/hotel-listing/hotel-listing.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 
 import { HotelListing } from '../model/hotel-listing.model';
-
-
+import {NgbDate} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-hotel-listing',
@@ -15,6 +14,16 @@ export class HotelListingComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var date1 =  new NgbDate(2020,19,22);
+    var date2 = new NgbDate(2020,19,23);
+    var obj1 = new HotelListing("San mateo","California",date1,date2,"",""); 
+    var obj2 = new HotelListing("San Jose","California",date1,date2,"",""); 
+    var obj3 = new HotelListing("San Francisco","California",date1,date2,"",""); 
+
+    this.hotelListing.push(obj1);
+    this.hotelListing.push(obj2);
+    this.hotelListing.push(obj3);
+
     
 
 
