12bae6dfb547a5bb63dda08ef2059c47fbf98d24
Pranali Narkar <pnarkar19@gmail.com>
2022-04-14 17:25:29 -0400

    Added Cypress tests

--- client/cypress/integration/flight.spec.ts
+++ client/cypress/integration/flight.spec.ts
@@ -32,7 +32,20 @@ describe("Flight page", () => {
     cy.get("input[id='validationDefault02']").type("Narkar")
     cy.get("select").select("Female")
     cy.get("input[id='invalidCheck2']").click()
+    cy.get("button[type='submit']").click()
+  });
 
+  it.only("Enters Credit Card Information", () => {
+    cy.get("input[name='address']").type("Univeristy of Florida")
+    cy.get("input[name='City']").type("Gainesville")
+    cy.get("input[id='State']").type("Florida")
+    cy.get("input[id='zip']").type("32601")
+    cy.get("input[name='cardname']").type("Pranali")
+    cy.get("input[name='cardnumber']").type("1569-1234-5678-2091")
+    cy.get("input[id='expmonth']").type("March")
+    cy.get("input[id='expyear']").type("2023")
+    cy.get("input[id='cvv']").type("123")
+    cy.get("input[id='paynow']").click()
   });
 
 });
