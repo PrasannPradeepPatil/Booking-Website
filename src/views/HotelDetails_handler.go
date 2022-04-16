0de2d5ed24a00cf054dd14657686e07e99d012e0
ahamad-s <92518161+ahamad-s@users.noreply.github.com>
2022-04-16 19:00:45 -0400

    Update HotelDetails_handler.go
    Updated as per added fields

--- src/views/HotelDetails_handler.go
+++ src/views/HotelDetails_handler.go
@@ -21,11 +21,15 @@ func HotelDetails(db *gorm.DB) gin.HandlerFunc {
 		}
 
 		log.Println("ID : ", req.ID)
-		db.Raw("select city,state,Hotelname,rating,StandardPrice,Address,Amenities from HotelData where ID = ?", req.ID).Scan(&res)
+		db.Raw("select city,state,Hotelname,rating,StandardPrice,Address,Amenities,ID from HotelData where ID = ?", req.ID).Scan(&res)
 
 		log.Println("response value : ", res.City+" "+res.State+" "+res.HotelName+" "+res.Address+" "+res.Rating+" "+res.Amenities)
+		if res.City == "" {
+			c.JSON(http.StatusOK, "No hotel details available for this ID")
+		} else {
+			c.JSON(http.StatusOK, res)
+		}
 
-		c.JSON(http.StatusOK, res)
 	}
 
 	return gin.HandlerFunc(fn)
