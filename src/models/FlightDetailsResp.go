4ffa12a9d6a6338b33977f0ec253695d2c9c6601
ahamad-s <92518161+ahamad-s@users.noreply.github.com>
2022-04-16 18:31:48 -0400

    Update FlightDetailsResp.go

--- src/models/FlightDetailsResp.go
+++ src/models/FlightDetailsResp.go
@@ -1,20 +1,18 @@
 package models
 
 type FlightDetailsResp struct {
-	Sourceairport          string `gorm:"not_null" form:"sourceairport" json:"sourceairport"`
-	Destinationairport     string `gorm:"not_null" form:"Destinationairport" json:"Destinationairport"`
-	Sourcecity             string `gorm:"not_null" form:"Sourcecity" json:"Sourcecity"`
-	Destinationcity        string `gorm:"not_null" form:"Destinationcity" json:"Destinationcity"`
-	Sourceairportcode      string `gorm:"not_null" form:"Sourceairportcode" json:"Sourceairportcode"`
-	Destinationairportcode string `gorm:"not_null" form:"Destinationairportcode" json:"Destinationairportcode"`
-	Flightnumber           string `gorm:"not_null" form:"Flightnumber" json:"Flightnumber"`
-	Departuretime          string `gorm:"not_null" form:"Departuretime" json:"Departuretime"`
-	Arrivaltime            string `gorm:"not_null" form:"Arrivaltime" json:"Arrivaltime"`
-	Price                  string `gorm:"not_null" form:"Price" json:"Price"`
-	Duration               string `gorm:"not_null" form:"Duration" json:"Duration"`
-	Flightname             string `gorm:"not_null" form:"Flightname" json:"Flightname"`
-	Boardingtime           string `gorm:"not_null" form:"Boardingtime" json:"Boardingtime"`
-	Checkinluggage         string `gorm:"not_null" form:"Checkinluggage" json:"Checkinluggage"`
-	Cabin                  string `gorm:"not_null" form:"Cabin" json:"Cabin"`
-	Cancellation           string `gorm:"not_null" form:"Cancellation" json:"Cancellation"`
+	Sourceairport          string `gorm:"not_null" form:"sourceairport" json:"sourceAirport"`
+	Destinationairport     string `gorm:"not_null" form:"Destinationairport" json:"destinationAirport"`
+	Sourcecity             string `gorm:"not_null" form:"Sourcecity" json:"sourceCity"`
+	Destinationcity        string `gorm:"not_null" form:"Destinationcity" json:"destinationCity"`
+	Sourceairportcode      string `gorm:"not_null" form:"Sourceairportcode" json:"sourceAirportCode"`
+	Destinationairportcode string `gorm:"not_null" form:"Destinationairportcode" json:"destinationAirportCode"`
+	Flightnumber           string `gorm:"not_null" form:"Flightnumber" json:"flightNumber"`
+	Departuretime          string `gorm:"not_null" form:"Departuretime" json:"departureTime"`
+	Arrivaltime            string `gorm:"not_null" form:"Arrivaltime" json:"arrivalTime"`
+	Price                  string `gorm:"not_null" form:"Price" json:"price"`
+	Duration               string `gorm:"not_null" form:"Duration" json:"duration"`
+	Flightname             string `gorm:"not_null" form:"Flightname" json:"flightName"`
+	Boardingtime           string `gorm:"not_null" form:"Boardingtime" json:"boardingTime"`
+	ID                     string `gorm:"not_null" form:"ID" json:"id"`
 }
