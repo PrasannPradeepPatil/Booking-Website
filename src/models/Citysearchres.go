1ca4321fccfea7b7794f2b2d81a43072f0dde0dc
ahamad-s <92518161+ahamad-s@users.noreply.github.com>
2022-04-16 17:23:45 -0400

    Update Citysearchres.go
    json field nomenclature update

--- src/models/Citysearchres.go
+++ src/models/Citysearchres.go
@@ -1,6 +1,6 @@
 package models
 
 type CitySearchRes struct {
-	City  string `gorm:"not_null" form:"City" json:"City"`
-	State string `gorm:"not_null" form:"State" json:"State"`
+	City  string `gorm:"not_null" form:"City" json:"city"`
+	State string `gorm:"not_null" form:"State" json:"state"`
 }
