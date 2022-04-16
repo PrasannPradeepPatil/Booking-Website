97dc1e903da063227811b0140704f34b88ca14a9
ahamad-s <92518161+ahamad-s@users.noreply.github.com>
2022-04-16 17:31:17 -0400

    Update PriceRes.go
    json filed nomenclature updated.

--- src/models/PriceRes.go
+++ src/models/PriceRes.go
@@ -1,6 +1,6 @@
 package models
 
 type PriceRes struct {
-	StandardPrice string `gorm:"not_null" form:"StandardPrice" json:"StandardPrice"`
-	FlexiblePrice string `gorm:"not_null" form:"FlexiblePrice" json:"FlexiblePrice"`
+	StandardPrice string `gorm:"not_null" form:"StandardPrice" json:"standardPrice"`
+	FlexiblePrice string `gorm:"not_null" form:"FlexiblePrice" json:"flexiblePrice"`
 }
