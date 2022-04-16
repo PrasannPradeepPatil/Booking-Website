7f77a5c8383d1a62ac76cc6b9054be3e1b5664b1
ahamad-s <92518161+ahamad-s@users.noreply.github.com>
2022-04-16 17:31:51 -0400

    Update SrchArptAPI.go
    json field nomenclature updated.

--- src/models/SrchArptAPI.go
+++ src/models/SrchArptAPI.go
@@ -1,8 +1,8 @@
 package models
 
 type SrchArptAPI struct {
-	Cityname    string `gorm:"not_null" form:"cityName" json:"cityname"`
-	Airportcode string `gorm:"not_null" form:"airportCode" json:"airportcode"`
-	Airportname string `gorm:"not_null" form:"airportName" json:"airportname"`
-	Countryname string `gorm:"not_null" form:"countryName" json:"countryname"`
+	Cityname    string `gorm:"not_null" form:"cityName" json:"cityName"`
+	Airportcode string `gorm:"not_null" form:"airportCode" json:"airportCode"`
+	Airportname string `gorm:"not_null" form:"airportName" json:"airportName"`
+	Countryname string `gorm:"not_null" form:"countryName" json:"countryName"`
 }
