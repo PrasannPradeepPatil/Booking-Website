87e5ce2dfe5d88d160affc9f0762c6146aa64332
Manish Alluri <33183210+manish-alluri@users.noreply.github.com>
2022-04-20 21:43:51 -0400

    Update FlightConfirmationRes.go

--- src/models/FlightConfirmationRes.go
+++ src/models/FlightConfirmationRes.go
@@ -4,5 +4,5 @@ type FlightConfirmationRes struct {
 	Emailstatus string `gorm:"not_null" form:"Emailstatus" json:"emailStatus"`
 	Apistatus   string `gorm:"not_null" form:"Apistatus" json:"apiStatus"`
 	Errorcode   string `gorm:"not_null" form:"Errorcode" json:"errorCode"`
-	Referencenumber string `gorm:"not_null" form:"ReferenceNumber" json:"referenceNumber"`
+	ReferenceNumber string `gorm:"not_null" form:"ReferenceNumber" json:"referenceNumber"`
 }
